e1f69ad6f090c5f83bd7cff1460ee26caf98baee
Logan Brunner <lgbrunner@gmail.com>
2021-10-28 11:04:51 +0200

    Copy text from existing site for content. Add some phosample photos to use.

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import './assets/fomantic/dist/semantic.css';
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Container, Divider, Header, Segment } from 'semantic-ui-react';
 
 function App() {
   return (
@@ -13,6 +13,23 @@ function App() {
         </p>
         <p>
           We are working on finding the best way how to present these pictures... This is just the bare minimum. A better(?) slideshow-style presentation is here, but it might be broken, as it is work in progress.
+        </p>
+        <Divider />
+        <Header as='h2'>
+          Calibration
+        </Header>
+        <Header as='h3'>
+          Data missing, we need your help!
+        </Header>
+        <p>
+          We need info about what pictures show low/med/high. So if you have an opinion about the flow you see in the pictures, please <a href='mailto:toro@kanuschle.ch'>tell us</a>.
+          <br />
+          Or, tell us when you paddled there and how you experienced that level.
+          <br />
+          Wishes, advice, contributions welcome too ;-)
+        </p>
+        <p>
+          We think <a href='https://redpig.ch/webcams/wassen/calibration_images/daymax-Wassenphoto_2021-07-12_20-24-51.jpg'>this is somewhere in the medium range</a>. Do you agree? And what about low and high?
         </p>
       </Segment>
     </div>
