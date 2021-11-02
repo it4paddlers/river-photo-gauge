5fff31d8d8a63d0477a0242eb9b7fc865e49a723
sirtet <toro@kanuschule.ch>
2021-11-02 18:48:40 +0100

    Update index.php
    fix default date,
    add Github link
    add available range
    add internet-down notice

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if(isset($_GET['newest'])){
 if(isset($_GET['oldest'])){
   $oldestRequested_ts = strtotime($_GET['oldest']);
 }else{
-  $oldestRequested_ts = time() - DEFAULTRANGE;
+  $oldestRequested_ts = $newestFile_ts - DEFAULTRANGE;
 };
 //TODO: would be nice to decode the doublecolon in the url for the viewer, see https://stackoverflow.com/a/40256517/1331544
 //var_dump($oldest_ts);die;
@@ -116,9 +116,11 @@ $oldestRequested_date = date('Y-m-d\TH:i', $oldestRequested_ts);
     Wishes, advice, contributions welcome too ;-) </p>
     <p>We think <a href="calibration_images/daymax-Wassenphoto_2021-07-12_20-24-51.jpg">this is somewhere in the medium range</a>. Do you agree? And what about low and high?</p>
     <h2>Date Range</h2>
+    <p>Images Available from <strong><?=date("M. d. Y",$oldestFile_ts)?></strong> to <strong><?=date("M. d. Y",$newestFile_ts)?></strong></p>
+    <div style="border:1px solid red">Internet in Wassen is broken since oct. 26th till maybe mid. nov. 2021.</div><br><br>
     <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
       <fieldset>
-        <legend>Select</legend>
+        <legend>Select Range</legend>
         <label for="oldest">Oldest</label>
           <input type="datetime-local" id="oldest" name="oldest" value="<?=$oldestRequested_date?>" min="<?=$oldestFile_date?>" max="<?=$newestFile_date?>">
         <label for="newest">Newest</label>
@@ -138,5 +140,6 @@ $oldestRequested_date = date('Y-m-d\TH:i', $oldestRequested_ts);
     }
 ?>
     </div>
+<a href="https://github.com/it4paddlers/river-photo-gauge" class="github-corner" aria-label="View source on GitHub"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#64CEAA; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>
   </body>
 </html>
