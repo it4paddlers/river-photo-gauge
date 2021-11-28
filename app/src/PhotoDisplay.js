a5fa6e1f2d15b006d42417b19fb30670de408561
Logan Brunner <logan.brunner@tno.nl>
2021-11-28 20:43:03 +0100

    Add date and time to top of picture.

--- app/src/PhotoDisplay.js
+++ app/src/PhotoDisplay.js
@@ -3,20 +3,21 @@ import React, { Component } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { Button, Container, Grid, Image, Segment } from 'semantic-ui-react';
-import wassen from './assets/images/Wassenphoto_2021-10-26_10-36-00.jpeg'
-import wassen1 from './assets/images/Wassenphoto_2021-10-26_11-36-00.jpeg'
-import wassen2 from './assets/images/Wassenphoto_2021-10-26_12-36-00.jpeg'
-import wassen3 from './assets/images/Wassenphoto_2021-10-26_13-36-00.jpeg'
-import wassen4 from './assets/images/Wassenphoto_2021-10-26_14-36-00.jpeg'
-import wassen5 from './assets/images/Wassenphoto_2021-10-26_15-36-00.jpeg'
-import wassen6 from './assets/images/Wassenphoto_2021-10-26_16-36-00.jpeg'
+import {Button, Container, Grid, Header, Image, Segment} from 'semantic-ui-react';
+import wassen from './assets/images/Wassenphoto_2021-10-26_10-36-00.jpeg';
+import wassen1 from './assets/images/Wassenphoto_2021-10-26_11-36-00.jpeg';
+import wassen2 from './assets/images/Wassenphoto_2021-10-26_12-36-00.jpeg';
+import wassen3 from './assets/images/Wassenphoto_2021-10-26_13-36-00.jpeg';
+import wassen4 from './assets/images/Wassenphoto_2021-10-26_14-36-00.jpeg';
+import wassen5 from './assets/images/Wassenphoto_2021-10-26_15-36-00.jpeg';
+import wassen6 from './assets/images/Wassenphoto_2021-10-26_16-36-00.jpeg';
 
 class PhotoDisplay extends Component{
   constructor(props) {
     super(props);
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
+    this.getPhotoDateTime = this.getPhotoDateTime.bind(this);
   }
 
   next() {
@@ -27,15 +28,22 @@ class PhotoDisplay extends Component{
     this.slider.slickPrev();
   }
 
+  getPhotoDateTime(photoUrl) {
+    const filename = photoUrl.split(/[/]/).pop();
+    const [date, time] = filename.split(/[_.]+/).slice(1, 3);
+    const timeFormatted = time.split(/[-]/).join(':');
+    return date + ', ' + timeFormatted;
+  }
+
   render() {
-    var settings = {
+    const settings = {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1
     };
 
-    var photoList = [wassen, wassen1, wassen2, wassen3, wassen4, wassen5, wassen6]
+    const photoList = [wassen, wassen1, wassen2, wassen3, wassen4, wassen5, wassen6]
 
     return (
       <React.Fragment>
@@ -44,10 +52,12 @@ class PhotoDisplay extends Component{
             <Grid.Row>
               <Grid.Column textAlign='center'>
                 <Slider ref={c => (this.slider = c)} {...settings}>
-                  {photoList.map(photo => {
+                  {photoList.map((photo, i) => {
+                    const dateTime = this.getPhotoDateTime(photo);
                     return (
-                      <div>
-                        <Image centered size='large' src={wassen} />
+                      <div key={i}>
+                        <Header as='h4'>{dateTime}</Header>
+                        <Image centered size='large' src={photo} />
                       </div>
                     )
                   })}
@@ -61,7 +71,7 @@ class PhotoDisplay extends Component{
                 </Button>
               </Grid.Column>
               <Grid.Column width={8} textAlign='left'>
-                <Button onClick={this.next}>
+                <Button small onClick={this.next}>
                   Next
                 </Button>
               </Grid.Column>
