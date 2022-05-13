67ccf2821c74f5aaa133efb12fa2b3b09ce1fd61
doomsower <K.Kuznetcov@gmail.com>
2022-05-13 08:08:35 +0300

    build: add husky, lint-staged and commitlint

--- app/lint-staged.config.js
+++ app/lint-staged.config.js
@@ -2,7 +2,6 @@ module.exports = {
   '*.{ts,tsx,css}': ['prettier . --write'],
   '*.{ts,tsx}': [
     'eslint . --cache --fix --ext .tsx --ext .ts',
-    () => 'yarn tsc',
-    () => 'yarn jest',
+    'pnpm typecheck',
   ],
 };
